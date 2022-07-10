1873fc7505c8cc411a7071bd5df8b4e1187ee134
KathouQC(GirlsPowers) <71993497+Kathou99@users.noreply.github.com>
2022-07-09 20:11:50 -0400

    Update config.php

--- api/contact/config.php
+++ api/contact/config.php
@@ -1,4 +1,4 @@
 <?php
-$adminEmail =  "hello@malith.dev"; //Please change this to yours
+$adminEmail =  "info@qliferoleplay.ca"; //Please change this to yours
 $SendMailFailederrorMessage = "Something went wrong";
 $SendMailEmptyerrorMessage = "Empty form";
